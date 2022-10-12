a06afdba47d93c1b4b582237ee1c880313919b4d
sairevathy23 <sairevathy23@gmail.com>
2022-10-12 18:31:31 +0000

    updated

--- terraform/ec2_creation/providers.tf
+++ terraform/ec2_creation/providers.tf
@@ -1,24 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "4.34.0"
-    }
-    github = {
-      source = "integrations/github"
-      version = "5.3.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region                  = "us-west-2"
+  region = "us-west-2"
   shared_credentials_files = ["/home/jenkins/.aws/credentials"]
-  profile                 = "dev"
+  profile = "dev" 
 }
 
-provider "github" {
-  token = "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"
-  owner = "sairevathy23"
-}
+
--- terraform/ec2_creation/s3bucket.tf
+++ terraform/ec2_creation/s3bucket.tf
@@ -1,19 +1,13 @@
-data "github_repository_file" "git-file" {
-  provider   = github
-  repository = "sairevathy23/devops"
-  file       = "Saibaba.jpg"
-}
- 
 resource "aws_s3_bucket" "ec2s3" {
-    name = ec2s3bucket1
+    bucket = "ec2s3bucket1"
     tags = {
     Name        = "jenkins-ansible"
     Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_object" "upload" {
+resource "aws_s3_object" "upload" {
   bucket = "${aws_s3_bucket.ec2s3.id}"
   key = "s3-Saibaba.jpg"
-  content = data.github_repository_file.git-file.content
+  source = "/home/jenkins/devops/Saibaba.jpg"
 }
