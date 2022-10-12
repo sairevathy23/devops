25c3e1ef379509aa4e45e688aedaab4e952097b1
sairevathy23 <sairevathy23@gmail.com>
2022-10-12 17:26:08 +0000

    updated providers

--- terraform/ec2_creation/providers.tf
+++ terraform/ec2_creation/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.34.0"
     }
+    github = {
+      source = "integrations/github"
+      version = "5.3.0"
+    }
   }
 }
 
@@ -12,4 +16,9 @@ provider "aws" {
   region                  = "us-west-2"
   shared_credentials_files = ["/home/jenkins/.aws/credentials"]
   profile                 = "dev"
+}
+
+provider "github" {
+  token = "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"
+  owner = "sairevathy23"
 }
